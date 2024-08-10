3e93e656b60c02cb0893ed0b446276b8d4554d49
Dean Kinyua <kinyuadean@gmail.com>
2024-08-10 15:58:22 +0300

    injecting numbers into the AST using unquote

---
+++ lib/metaprograming/math.exs
@@ -0,0 +1,23 @@
+defmodule Metaprograming.Math do
+  # {:+, [context: Elixir, import: Kernel], [5, 2]}-
+  defmacro say({:+, _, [lhs, rhs]}) do
+    quote do
+      lhs = unquote(lhs)
+      rhs = unquote(rhs)
+      result = lhs + rhs
+      IO.puts("#{lhs} plus #{rhs} is #{result}")
+      result
+    end
+  end
+
+  # {:*, [context: Elixir, import: Kernel], [8, 3]}-
+  defmacro say({:*, _, [lhs, rhs]}) do
+    quote do
+      lhs = unquote(lhs)
+      rhs = unquote(rhs)
+      result = lhs * rhs
+      IO.puts("#{lhs} times #{rhs} is #{result}")
+      result
+    end
+  end
+end
--- test/metaprograming_web/controllers/error_json_test.exs
+++ test/metaprograming_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule MetaprogramingWeb.ErrorJSONTest do
   use MetaprogramingWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert MetaprogramingWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert MetaprogramingWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
