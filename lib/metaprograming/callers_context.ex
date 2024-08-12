5968abc85127518220c827df9024396e24516931
Dean Kinyua <kinyuadean@gmail.com>
2024-08-12 21:22:25 +0300

    done

--- lib/metaprograming/callers_context.ex
+++ lib/metaprograming/callers_context.ex
@@ -1,12 +1,13 @@
 defmodule Mod do
   defmacro definfo do
+    # dbg(__ENV__)
     # * Returns the Macro's definition context
     IO.puts("In macro's context (#{__MODULE__}).")
-# * Some additional information about the definition context
-    IO.puts("""
-    Definition context of the macro is #{__MODULE__}
-    The macros in definition are #{inspect(__MODULE__.__info__(:macros))}
-    """)
+    # * Some additional information about the definition context
+    # IO.puts("""
+    # Definition context of the macro is #{__MODULE__}
+    # The macros in definition are #{inspect(__MODULE__.__info__(:macros))}
+    # """)
 
     quote do
       # * We are now inside the caller context
@@ -26,3 +27,12 @@ defmodule MyModule do
   require Mod
   Mod.definfo()
 end
+
+# ast =(
+#   quote do
+#     if meaning_to_life == 42 do
+#       "it's true"
+#     else
+#       "it remains to be seen"
+#     end
+#   end)
