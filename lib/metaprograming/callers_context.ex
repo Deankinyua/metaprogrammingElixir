e3481550890d6f816b7d960c55f70412477c87ca
Dean Kinyua <kinyuadean@gmail.com>
2024-08-12 17:30:46 +0300

    you cannot call a function which depends on __MODULE__ from the module lexical environment as the module does not yet exist as it has not yet been compiled . __MODULE__.info

---
+++ lib/metaprograming/callers_context.ex
@@ -0,0 +1,28 @@
+defmodule Mod do
+  defmacro definfo do
+    # * Returns the Macro's definition context
+    IO.puts("In macro's context (#{__MODULE__}).")
+# * Some additional information about the definition context
+    IO.puts("""
+    Definition context of the macro is #{__MODULE__}
+    The macros in definition are #{inspect(__MODULE__.__info__(:macros))}
+    """)
+
+    quote do
+      # * We are now inside the caller context
+      IO.puts("In caller's context (#{__MODULE__}).")
+
+      def friendly_info do
+        IO.puts("""
+        My name is #{__MODULE__}
+        My functions are #{inspect(__MODULE__.__info__(:functions))}
+        """)
+      end
+    end
+  end
+end
+
+defmodule MyModule do
+  require Mod
+  Mod.definfo()
+end
