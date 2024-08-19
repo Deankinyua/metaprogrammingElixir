cdc2cacc2bb818b354cdbea3ba653c73b718fbc2
Dean Kinyua <kinyuadean@gmail.com>
2024-08-19 05:16:36 +0300

    use bind_quoted to evoke twice

--- lib/metaprograming/assertion.ex
+++ lib/metaprograming/assertion.ex
@@ -3,6 +3,9 @@ defmodule Metaprograming.Assertion do
   # {:==, [context: Elixir, import: Kernel], [5, 5]}
   # * bind_quoted option passes a binding to the block, ensuring
   # * that the outside bound variables are unquoted only a single time.
+
+  # * The goal for our assert macro is to accept a left-hand side and right-hand side
+  # * expression, separated by an Elixir operator, such as assert 1 > 0.
   defmacro assert({operator, _, [lhs, rhs]}) do
     quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
       Assertion.Test.assert(operator, lhs, rhs)
--- lib/metaprograming/debugger.ex
+++ lib/metaprograming/debugger.ex
@@ -1,19 +1,26 @@
 defmodule Metaprograming.Debugger do
   # note: exploring the importance of bind_quoted
+
+  # *Important: if you are reading the application environment at compilation time,
+  # * for example, inside the module definition instead of inside of a function,
+  # * see compile_env!/2 instead.
   defmacro log(expression) do
-    # * gets the value for the key :log_level
+    # * gets the value for the key :log_level of the application debugger
+    # * The function call remote_api_call.() will be evoked twice
     if Application.get_env(:debugger, :log_level) == :debug do
-      quote do
+      quote bind_quoted: [expression: expression] do
         IO.puts("=================")
-        IO.inspect(unquote(expression))
+        IO.inspect(expression)
         IO.puts("=================")
-        unquote(expression)
+        expression
       end
     else
       expression
     end
   end
 end
+
+# expression = "calling the remote api"
 
 # note: The following quoted expressons are equivalent
 # quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
