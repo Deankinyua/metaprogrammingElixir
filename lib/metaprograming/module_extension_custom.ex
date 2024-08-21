94fe51028734240d45d81a09b370284e71c2bcd7
Dean Kinyua <kinyuadean@gmail.com>
2024-08-21 10:27:41 +0300

    Since __using__/1 is typically a macro, all the usual macro rules apply, and its return value should be quoted code that is then inserted where use/2 is called.

--- lib/metaprograming/module_extension_custom.ex
+++ lib/metaprograming/module_extension_custom.ex
@@ -1,5 +1,5 @@
-defmodule Assertion do
-  defmacro extend(_options \\ []) do
+defmodule AssertionExtension do
+  defmacro __using__(_options) do
     quote do
       import unquote(__MODULE__)
 
@@ -11,6 +11,10 @@ defmodule Assertion do
 end
 
 defmodule MathTest do
-  require Assertion
-  Assertion.extend()
+  # * use SomeModule simply invokes the SomeModule.__using__/1 macro
+  # * passing the second argument of use as the argument to SomeModule.__using__/1
+
+  # Since __using__/1 is typically a macro, all the usual macro rules apply,
+  #  and its return value should be quoted code that is then inserted where use/2 is called.
+  use AssertionExtension
 end
