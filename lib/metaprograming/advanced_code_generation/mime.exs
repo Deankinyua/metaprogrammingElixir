c056ee4f9bf3769aebda6874b8943e9432322d20
Dean Kinyua <kinyuadean@gmail.com>
2024-08-28 20:58:50 +0300

    \t brings confusion

--- lib/metaprograming/advanced_code_generation/mime.exs
+++ lib/metaprograming/advanced_code_generation/mime.exs
@@ -1,7 +1,7 @@
 defmodule Mime do
   for line <- File.stream!(Path.join([__DIR__, "mimes.txt"]), [], :line) do
-    [type, rest] = line |> String.split("\t") |> Enum.map(&String.trim(&1))
-    extensions = String.split(rest, ~r/,\s?/)
+    [type, rest] = line |> String.split() |> Enum.map(&String.trim(&1))
+    extensions = String.split(rest)
 
     def exts_from_type(unquote(type)), do: unquote(extensions)
     def type_from_ext(ext) when ext in unquote(extensions), do: unquote(type)
@@ -11,3 +11,9 @@ defmodule Mime do
   def type_from_ext(_ext), do: nil
   def valid_type?(type), do: exts_from_type(type) |> Enum.any?()
 end
+
+
+# "Dean kamanu kinyua"
+
+# "application/javascript .js"
+# "hahahha jajajja  jsjjsjjsj kakka "
